1d823c1a8a0cc57b8eb932ed230e92a2cc318f65
vendorsold <vendors@sold.com.sg>
2025-05-19 23:30:44 +0800

    Refined build script to set lang='en'

--- add-lang.js
+++ add-lang.js
@@ -4,8 +4,8 @@ async function addLangAttribute() {
   try {
     const results = await replaceInFile({
       files: './**/*.html',
-      from: /<html>/gi,
-      to: '<html lang="en">',
+      from: /<html\b([^>]*)>/gi,
+      to: '<html lang="en"$1>',
     });
     console.log('Added lang="en" to:', results.filter((r) => r.hasChanged).map((r) => r.file));
   } catch (error) {
